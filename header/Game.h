78dc2e7285284130c3f7e4d68d348dc8d7ddca52
MLOTKOWSKI DIMITRI <dimitri.mlotkowski@etu.unistra.fr>
2019-03-02 02:55:54 +0100

    Preliminary work for the Game class

--- header/Game.h
+++ header/Game.h
@@ -5,5 +5,33 @@
 
 class Game {
 private:
+	// Attributes
+	static int nextId; // Initializing the static variable in a header doesn't seem to be possible
+	int id;
+	int tokens[4];
+	int totalExchangedSets;
+	int activePlayer;
+	int phase;
+	bool territoryCapture;
+	bool running;
+	int nbPlayer;
+	Map map;
+	// board : list of TerritoryState
+	// players : list of Player
+	int lastAttackedTerritory;
+	bool lastAttackCapture;
+
+	//Methods
+	void start();
+	void nextPlayer();
+	void nextPhase();
+	void chooseFirstPlayer();
+	int useSet(int tok1, int tok2, int tok3);
+	void solveCombat();
+	void moveUnits(int source, int destination, int units);
+	void setInitialReinforcement();
+	void end();
+	int updatePlayersStatsInDB();
+
 public:
 };
