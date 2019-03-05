e4dfd5ea7e6acae780be419415332feb4b5d534d
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-05 23:03:16 +0100

    added comments to the GameServer class methods in the source file

--- source/GameServer.cpp
+++ source/GameServer.cpp
@@ -26,7 +26,10 @@ void GameServer::treatMessage(std::string message)
 
 void GameServer::createGame(std::string mapName, std::vector<std::string> playersNames)
 {
+    /* create a new game */
     Game newGame(mapName, playersNames, MAX_PLAYERS);
+
+    /* copy the new object into the games list */
     games.push_back(newGame);
 }
 
@@ -34,10 +37,13 @@ int GameServer::destroyGame(int id)
 {
     bool found = false;
 
+    /* for each game in the games list .. */
     for (int i = 0; i < games.size(); i++)
     {
+        /* .. we test the id .. */
         if (games[i].getId() == id)
         {
+            /* .. and we erase it if we find it */
             games.erase(games.begin() + i);
             found = true;
         }
