f32c50effb1d965a0a470bc8961167c2e6e9e439
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-06 11:56:28 +0100

    modified destroyGame method of GameServer class

--- source/GameServer.cpp
+++ source/GameServer.cpp
@@ -9,36 +9,41 @@
 
 std::vector<Game> GameServer::games;
 
+
 int GameServer::main()
 {
     return 0;
 }
+
 
 void GameServer::listen()
 {
 
 }
 
+
 void GameServer::treatMessage(std::string message)
 {
 
 }
 
+
 void GameServer::createGame(std::string mapName, std::vector<std::string> playersNames)
 {
     /* create a new game */
-    Game newGame(mapName, playersNames, MAX_PLAYERS);
+    Game newGame(mapName, playersNames[0], MAX_PLAYERS);
 
     /* copy the new object into the games list */
     games.push_back(newGame);
 }
+
 
 int GameServer::destroyGame(int id)
 {
     bool found = false;
 
     /* for each game in the games list .. */
-    for (int i = 0; i < games.size(); i++)
+    for (int i = 0; i < games.size() && !found; i++)
     {
         /* .. we test the id .. */
         if (games[i].getId() == id)
