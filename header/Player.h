cb38c106bb2f5fb68fd1613fc2a268f6c518c206
Pambou moubogha  eddy <eddy-vianney.pambou-mouboghia@etu.unistra.fr>
2019-03-06 16:27:42 +0100

    implementation Player class

--- header/Player.h
+++ header/Player.h
@@ -2,13 +2,31 @@
 #define PLAYER_H
 
 #include <string>
-using namespace std;
+#include <vector>
 
 class Player {
-private:
 public:
-    Player(){}
-    Player(string name){}
+	std::string name;
+	std::vector<int> tokens = std::vector<int>(4);
+    int reinforcement;
+	int victories;
+    int defeats;
+    int gained_territories;
+    int lost_territories;
+    int set_of_tokens[3];
+    bool isAlive;
+    //Player();
+    Player(std::string name);
+    bool set_is_valid(int tok1, int tok2, int tok3);
+	void hasSet(int tok1, int tok2, int tok3);
+    std::vector<int>listTokens();
+    void removeAllTokens();
+    void receiveToken(int type);
+    void receiveTokens(int numberOfEachToken[4]);
+    void removeTokens(int numberOfEachToken[4]);
+    void give_reinforcement(int number_of_rein, int last_rein_val);
+    //~Player();
+private:
 };
 
-#endif //PLAYER_H
+#endif 
