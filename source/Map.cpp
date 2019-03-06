b3dae6dc5aecbf248414071d497304bc4c9d875a
Raphael Schwehr <raphael.schwehr@etu.unistra.fr>
2019-03-06 16:11:30 +0100

    changing 'int i' to 'size_t i' in getMapIndex and exists

--- source/Map.cpp
+++ source/Map.cpp
@@ -86,7 +86,7 @@ vector<Territory> Map::getTerritories()
 bool Map::exists(string name)
 {
   bool res = false;
-  for(int i = 0;!res && i<Map::maps.size();i++)
+  for(size_t i = 0;!res && i<Map::maps.size();i++)
   {
     if(Map::maps.at(i).getName()==name)
       res = true;
@@ -97,7 +97,7 @@ bool Map::exists(string name)
 int Map::getMapIndex(string name)
 {
   int res = -1;
-  for(int i = 0; res == -1 && i<Map::maps.size();i++)
+  for(size_t i = 0; res == -1 && i<Map::maps.size();i++)
   {
     if(Map::maps.at(i).getName()==name)
       res=i;
