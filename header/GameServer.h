6bf2c2cc8c82bdf617505cc130ea667fe2d31ecf
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-10 12:24:08 +0100

    added websocket includes into the GameServer header file

--- header/GameServer.h
+++ header/GameServer.h
@@ -1,16 +1,20 @@
 #ifndef GAMESERVER_H
 #define GAMESERVER_H
 
+#include <iostream>
 #include <vector>
 #include <string>
 #include "../header/Game.h"
+
+#include "../libs/websocketpp/config/asio_no_tls.hpp"
+#include "../libs/websocketpp/server.hpp"
+
 
 class GameServer {
 private:
     static std::vector<Game> games;
 
 public:
-    // static int main();
     static void listen();
     static void treatMessage(std::string message);
     static void createGame(std::string mapName, std::vector<std::string> playersNames);
