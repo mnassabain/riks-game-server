d961ed4bc16cf315f0ec48e80268f1fffb693bfc
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-17 17:16:28 +0100

    Added message code enumeration

---
+++ header/MessageCode.h
@@ -0,0 +1,16 @@
+#ifndef MESSAGECODES_H
+#define MESSAGECODES_H
+
+
+typedef enum
+{
+    CODE_ERROR,
+    CODE_SIGN_UP,
+    CODE_CONNECT,
+    CODE_DISCONNECT
+
+} MessageCode;
+
+
+
+#endif // MESSAGE_CODES_H
