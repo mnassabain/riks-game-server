0fb9bb5da57f09e34698937d5e0be22a1e17b975
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-03-17 12:11:34 +0100

    cleaned up GameServer files

--- header/GameServer.h
+++ header/GameServer.h
@@ -11,9 +11,7 @@ using namespace std;
 #include "../libs/websocketpp/config/asio_no_tls.hpp"
 #include "../libs/websocketpp/server.hpp"
 
-
 #define SERVER_PORT 9002
-
 
 /* typedefs for server endpoint, message pointer and connection HDL */
 typedef websocketpp::server<websocketpp::config::asio> ServerEndpoint;
@@ -23,37 +21,32 @@ typedef websocketpp::connection_hdl Connection;
 
 class GameServer {
 private:
+    /* game data */
     static vector<Game> games;
 
-    /* sockets */
-    /* server endpoint */
+    /* server & socket data */
     static ServerEndpoint endpoint;
-
-    /* open connection list */
     static vector<Connection> connections;
 
-    /* message handler */
-    static void onMessage(Connection connection, Message msg);
-
-    /* open connection handler */
-    static void onOpenConnection(Connection connection);
-
-public:
-    static void listen();
-    static void treatMessage(string message);
-    static void createGame(string mapName, vector<string> playersNames);
-    static int destroyGame(int id);
-
-    /* sockets */
-    /* initialises the server endpoint */
+    /* server control methods */
     static void init();
-
-    /* listens to incoming messages */
     static void run();
-
-    /* stops server */
     static void stop();
 
+    /* connection & communication handlers */
+    static void onMessage(Connection connection, Message msg);
+    static void onOpenConnection(Connection connection);
+
+
+
+public:
+    /* server control methods */
+    static void listen();
+    static void treatMessage(string message);
+
+    /* game control methods */
+    static void createGame(string mapName, vector<string> playersNames);
+    static int destroyGame(int id);
 
     /* debugging */
     static int nbGames();
--- source/GameServer.cpp
+++ source/GameServer.cpp
@@ -1,21 +1,19 @@
 #include "../header/GameServer.h"
 
 /* debug */
-
 #define MAX_PLAYERS 6
 
-/* debug */
 
 
 vector<Game> GameServer::games;
-
 ServerEndpoint GameServer::endpoint;
 vector<Connection> GameServer::connections;
 
 
 void GameServer::listen()
 {
-
+    init();
+    run();
 }
 
 
@@ -40,7 +38,7 @@ int GameServer::destroyGame(int id)
     bool found = false;
 
     /* for each game in the games list .. */
-    for (size_t i = 0; i < games.size() && !found; i++)
+    for (unsigned int i = 0; i < games.size() && !found; i++)
     {
         /* .. we test the id .. */
         if (games[i].getId() == id)
@@ -55,25 +53,33 @@ int GameServer::destroyGame(int id)
 }
 
 
-/* sockets */
+/* PRIVATE METHODS */
 
 void GameServer::init()
 {
-    /* reuse 9002 address */
-    endpoint.set_reuse_addr(true);
+    try
+    {
+        /* reuse 9002 address */
+        endpoint.set_reuse_addr(true);
 
-    /* Set logging settings */ 
-    endpoint.set_error_channels(websocketpp::log::elevel::all); 
-    endpoint.set_access_channels(websocketpp::log::alevel::all ^
-        websocketpp::log::alevel::frame_payload);
+        /* Set logging settings */ 
+        endpoint.set_error_channels(websocketpp::log::elevel::all); 
+        endpoint.set_access_channels(websocketpp::log::alevel::all ^
+            websocketpp::log::alevel::frame_payload);
 
-    /* Initialize Asio */
-    endpoint.init_asio(); 
+        /* Initialize Asio */
+        endpoint.init_asio(); 
 
-    /* Register new message handler */
-    endpoint.set_message_handler(&onMessage);
+        /* Register new message handler */
+        endpoint.set_message_handler(&onMessage);
 
-    endpoint.set_open_handler(&onOpenConnection);
+        endpoint.set_open_handler(&onOpenConnection);
+    }
+    catch(websocketpp::exception e)
+    {
+        cout << "server init error: " << e.what() << endl;
+        exit(1);
+    }
 }
 
 
@@ -145,7 +151,8 @@ void GameServer::onOpenConnection(Connection connection)
 }
 
 
-/* debugging */
+/* DEBUGGING */
+
 int GameServer::nbGames()
 {
     return games.size();
