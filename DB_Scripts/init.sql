fd8e1427fa8e8a44ef971d854c401be1b57dc874
Raphael Schwehr <raphael.schwehr@etu.unistra.fr>
2019-03-19 20:46:27 +0100

    updated syntax to sqlite syntax

--- DB_Scripts/init.sql
+++ DB_Scripts/init.sql
@@ -1,38 +1,36 @@
 --users
 CREATE TABLE users(
-idu int(11) AUTO_INCREMENT,
+idu INTEGER PRIMARY KEY,
 login varchar(60) NOT NULL,
-password varchar(200) NOT NULL,
-constraint pk_users PRIMARY KEY (idu)
+password varchar(200) NOT NULL
 );
 
 --users' statistics
 CREATE TABLE stats(
-idu int(11) NOT NULL,
-victories int(11) NOT NULL,
-defeats int(11) NOT NULL,
-gained_territories int(11) NOT NULL,
-lost_territories int(11) NOT NULL,
-reinforcement int(11) NOT NULL,
-sets_of_tokens int(11) NOT NULL,
+idu INTEGER NOT NULL,
+victories INTEGER NOT NULL,
+defeats INTEGER NOT NULL,
+gained_territories INTEGER NOT NULL,
+lost_territories INTEGER NOT NULL,
+reinforcement INTEGER NOT NULL,
+sets_of_tokens INTEGER NOT NULL,
 CONSTRAINT pk_stats PRIMARY KEY(idu),
 CONSTRAINT fk_stats_users FOREIGN KEY(idu) references users(idu) on delete cascade
 );
 
 --games
 CREATE TABLE games(
-idg int(11) AUTO_INCREMENT,
-nbplayers int(11) NOT NULL,
+idg INTEGER PRIMARY KEY,
+nbplayers INTEGER NOT NULL,
 map_name varchar(60) NOT NULL,
-CONSTRAINT pk_games PRIMARY KEY(idg),
 CONSTRAINT ck_nbplayers CHECK (nbplayers >1 and nbplayers <7)
 );
 
 --games ranking
 CREATE TABLE ranking(
-idg int(11),
-idu int(11),
-place int(11) NOT NULL,
+idg INTEGER,
+idu INTEGER,
+place INTEGER NOT NULL,
 CONSTRAINT pk_ranking PRIMARY KEY (idg,idu),
 CONSTRAINT fk_ranking_games FOREIGN KEY(idg) references games(idg) on delete cascade,
 CONSTRAINT fk_ranking_users FOREIGN KEY(idu) references users(idu) on delete cascade
