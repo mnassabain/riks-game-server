f4a0594aa78cce0b0a1dff687e8197bb805a7842
MLOTKOWSKI DIMITRI <dimitri.mlotkowski@etu.unistra.fr>
2019-03-26 12:05:24 +0100

    Fixed an include and a comment line

--- header/Game.h
+++ header/Game.h
@@ -9,6 +9,7 @@
 #include "Player.h"
 #include "Map.h"
 #include "Structs.h"
+#include "MessageCode.h"
 
 #include "../libs/json/json.hpp"
 
@@ -18,7 +19,7 @@ using json = nlohmann::json;
 class Game {
 private:
 	// Attributes
-	// Lobby variables // Gotta remember to add a string name for the lobby and change the public constructor to reflect the change
+	// Lobby variables
 	static int nextId;
 	int id;
 	string name;
