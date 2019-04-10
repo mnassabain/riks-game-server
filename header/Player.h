d52b41206b6ff14dfd35a0c6dece7e8495995bb3
Raphael Schwehr <raphael.schwehr@etu.unistra.fr>
2019-04-10 15:36:13 +0200

    added Player documentation

--- header/Player.h
+++ header/Player.h
@@ -20,31 +20,134 @@ public:
 	int unitsLost;
     int setsSpent;
 
+    /**
+     * @brief Player : Constructor
+     * @param name : Player name
+     */
     Player(std::string name);
-	bool hasSet(int tok1, int tok2, int tok3);
+
+    /**
+     * @brief hasSet : Check if the player has a set of given tokens
+     * @param tok1 : type of token #1
+     * @param tok2 : type of token #2
+     * @param tok3 : type of token #3
+     * @return true if the player has the given set, false otherwise
+     */
+    bool hasSet(int tok1, int tok2, int tok3);
     /*std::vector<int>listTokens(); */
+
+    /**
+     * @brief listTokens : getter on Player's tokens
+     * @return int[4] containing the number of each type of token possessed
+     */
     int* listTokens();
+
+    /**
+     * @brief removeAllTokens : Set to 0 the number of each type of Player's token
+     */
     void removeAllTokens();
+
+    /**
+     * @brief receiveToken : add one token of the given type to the Player
+     * @param type : token's type
+     */
     void receiveToken(int type);
-	void removeToken(int type);
+
+    /**
+     * @brief removeToken : remove one token of the given type to the Player
+     * @param type : token's type
+     */
+    void removeToken(int type);
+
+    /**
+     * @brief revieveTokens : give the Player multiple tokens of different type
+     * @param numberOfEachToken : number of each token (index represents the token's type)
+     */
     void receiveTokens(int numberOfEachToken[4]);
+
+    /**
+     * @brief removeTokens : remove multiple tokens of different type to the Player
+     * @param numberOfEachToken : number of each token (index represents the token's type)
+     */
     void removeTokens(int numberOfEachToken[4]);
+
+    /**
+     * @brief getTerritoriesCaptured : getter
+     * @return number of terrirories captured
+     */
     int getTerritoriesCaptured();
+
+    /**
+     * @brief getTerritoriesLost : getter
+     * @return number of terrirories lost
+     */
     int getTerritoriesLost();
+   
+    /**
+     * @brief getSetsSpent : getter
+     * @return number of sets spent
+     */
     int getSetsSpent();
+
+    /**
+     * @brief addTerritoriesCaptured : add one to the number of territories captured
+     */
     void addTerritoriesCaptured();
+
+    /**
+     * @brief addTerritoriesCaptured : add one to the number of territories lost
+     */
     void addTerritoriesLost();
+
+    /**
+     * @brief addSetsSpent : add one to the number of sets spent
+     */
     void addSetsSpent();
+
+    /**
+     * @brief die : kill the player (isAlive = false)
+     */
     void die();
 
-	void addReinforcement(int reinforcement);
-	void spendReinforcement(int reinforcement);
-	int getReinforcement();
-	int getTerritoriesOwned();
-	void addTerritoriesOwned();
-	void subTerritoriesOwned();
+    /**
+     * @brief addReinforcement : add some reinforcement to the player
+     * @param reinforcement : number of reinforcement to add
+     */
+    void addReinforcement(int reinforcement);
+   
+    /**
+     * @brief spendReindorcement : spend some reinforcement of the player
+     * @param reinforcement : number of reinforcement to spend
+     */
+    void spendReinforcement(int reinforcement);
 
-	std::string getName();
+    /**
+     * @brief getReinforcement : getter
+     * @return number of reinforcement
+     */
+    int getReinforcement();
+
+    /**
+     * @brief getTerritoriesOwned : getter
+     * @return number of territoriesOwned
+     */
+    int getTerritoriesOwned();
+
+    /**
+     * @brief addTerritoriesOwned : add one to the number of territories owned by the player
+     */
+    void addTerritoriesOwned();
+
+    /**
+     * @brief subTerritoriesOwned : remove one to the number of territories owned by the player
+     */
+    void subTerritoriesOwned();
+
+    /**
+     * @brief getName : getter
+     * @return player's name
+     */
+    std::string getName();
 private:
 };
 
