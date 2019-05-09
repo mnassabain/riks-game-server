327bfc8db15002cbf420ae30cd1327b19f860f3a
NASSABAIN MARCO <marco.nassabain@etu.unistra.fr>
2019-05-09 11:05:29 +0200

    added launch server script

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "## START SCRIPT ##"
+
+echo "## MAKE ##"
+make
+
+exitcode=1
+
+while [ $exitcode -ne 0 ]
+do
+
+    if [ ! -f riks-server ]
+    then
+        echo "## EXECUTABLE NOT FOUND ##"
+        echo "## END SCRIPT ##"
+        exit 1
+    fi
+
+    echo "## LAUNCHING SERVER ##"
+
+    ./riks-server
+
+    exitcode=$?
+    echo "## SERVER EXITED WITH CODE $exitcode ##"
+
+done
+
+echo "## END SCRIPT ##"
